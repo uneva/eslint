5674bf901de4ee0836ace6dc34ee2ab63b31ac90
maggot_code <63178315+maggot-code@users.noreply.github.com>
2024-04-06 20:13:29 +0800

    fix: Show full rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,8 @@
 // import unlint from "./src/index.ts";
 import unlint from "./dist/index.js";
 
-export default unlint();
+export default unlint({
+    typescript: true,
+    vue: true,
+    unocss: true,
+});
