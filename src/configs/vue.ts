05b4d2ccfb32a403c12ad28ae9f29ceb686008c1
maggot-code <udp.pkg.loss@gmail.com>
2024-08-10 03:24:44 +0800

    chore: Update vue.ts to include StylisticFocus option in OptionsVue

--- src/configs/vue.ts
+++ src/configs/vue.ts
@@ -1,4 +1,4 @@
-import type { FlatConfigItem, OptionsVue } from "../types";
+import type { FlatConfigItem, OptionsVue, StylisticFocus } from "../types";
 
 import tseslint from "typescript-eslint";
 import { typescriptSetup } from "./typescript";
@@ -7,7 +7,9 @@ import { mergeProcessors } from "eslint-merge-processors";
 import { parserTypescript, parserVue, pluginVue, processorVueBlocks } from "../plugins";
 import { GLOB_VUE } from "../globs";
 
+const defaulTstylistic: StylisticFocus = { indent: 4 };
 export function vue(options?: OptionsVue): FlatConfigItem[] {
+    const { indent } = typeof options?.stylistic === "boolean" ? defaulTstylistic : options?.stylistic || defaulTstylistic;
     const files = Array.isArray(options?.files) ? options.files : [GLOB_VUE];
     const configs: FlatConfigItem[] = [];
 
@@ -84,6 +86,50 @@ export function vue(options?: OptionsVue): FlatConfigItem[] {
 
                 "vue/component-name-in-template-casing": ["error", "PascalCase"],
                 "vue/component-options-name-casing": ["error", "PascalCase"],
+                // this is deprecated
+                "vue/component-tags-order": "off",
+                "vue/custom-event-name-casing": ["error", "camelCase"],
+                "vue/define-macros-order": ["error", {
+                    order: ["defineOptions", "defineProps", "defineEmits", "defineSlots"],
+                }],
+                "vue/dot-location": ["error", "property"],
+                "vue/dot-notation": ["error", { allowKeywords: true }],
+                "vue/eqeqeq": ["error", "smart"],
+                "vue/html-indent": ["error", indent],
+                "vue/html-quotes": ["error", "double"],
+                "vue/max-attributes-per-line": "off",
+                "vue/multi-word-component-names": "off",
+                "vue/no-dupe-keys": "off",
+                "vue/no-empty-pattern": "error",
+                "vue/no-irregular-whitespace": "error",
+                "vue/no-loss-of-precision": "error",
+                "vue/no-restricted-syntax": [
+                    "error",
+                    "DebuggerStatement",
+                    "LabeledStatement",
+                    "WithStatement",
+                ],
+                "vue/no-restricted-v-bind": ["error", "/^v-/"],
+                "vue/no-setup-props-reactivity-loss": "off",
+                "vue/no-sparse-arrays": "error",
+                "vue/no-unused-refs": "error",
+                "vue/no-useless-v-bind": "error",
+                "vue/no-v-html": "off",
+                "vue/object-shorthand": [
+                    "error",
+                    "always",
+                    {
+                        avoidQuotes: true,
+                        ignoreConstructors: false,
+                    },
+                ],
+                "vue/prefer-separate-static-class": "error",
+                "vue/prefer-template": "error",
+                "vue/prop-name-casing": ["error", "camelCase"],
+                "vue/require-default-prop": "off",
+                "vue/require-prop-types": "off",
+                "vue/space-infix-ops": "error",
+                "vue/space-unary-ops": ["error", { nonwords: false, words: true }],
 
                 ...options?.stylistic
                     ? {
