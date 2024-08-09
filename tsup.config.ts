cca0444e9e73689a21fb88582f194912057256be
maggot-code <udp.pkg.loss@gmail.com>
2024-08-09 17:49:54 +0800

    chore: Update tsup.config.ts to include entry point and format options

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    format: ["esm", "cjs"],
+    shims: true,
+    dts: true,
+    clean: true,
+    minify: false,
+});
