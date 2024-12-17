4a5cb6f4e8683749759280c58ad755de040198ca
maggot-code <udp.pkg.loss@gmail.com>
2024-12-17 13:42:46 +0800

    fix: correct indentation in isInEditor check

--- src/state.ts
+++ src/state.ts
@@ -3,9 +3,9 @@ import { getPackageInfoSync, isPackageExists } from "local-pkg";
 
 export const isInEditor = !!(
     (process.env.VSCODE_PID
-    || process.env.VSCODE_CWD
-    || process.env.JETBRAINS_IDE
-    || process.env.VIM)
+        || process.env.VSCODE_CWD
+        || process.env.JETBRAINS_IDE
+        || process.env.VIM)
     && !process.env.CI
 );
 
